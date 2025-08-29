28f36f8eaf45b2fb44d3af1938d9d3288912ada8
MIKHAIL <hypericum90@gmail.com>
2025-08-29 04:15:03 +0400

    edit globalstyles and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Global } from '@emotion/react'; // Добавьте этот импорт
 import TodoApp from "./components/TodoApp/TodoApp";
-import GlobalStyles from "./styles/GlobalStyles";
+import { globalStyles } from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
+      <Global styles={globalStyles} />
       <TodoApp />
     </>
   );
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,11 +1,23 @@
+import { css } from '@emotion/react';
 
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyles = createGlobalStyle`
+export const globalStyles = css`
   body {
     margin: 0;
-    font-family: sans-serif;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #f5f5f5;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  #root {
+    width: 100%;
+    min-height: 100vh;
   }
 `;
-
-export default GlobalStyles;
